45fc81f8ae8b16ce1c536ff6dce30032744ee9f8
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-09-28 14:51:52 -0300

    ADD/modelos iniciales

---
+++ api/src/models/Item.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = sequelize => {
+  sequelize.define(
+    'item',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      total: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
