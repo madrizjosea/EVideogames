0dc7dae8148e6346fb759e43af4be2506a6a71e9
Razgrizsx <razgrisss41@gmail.com>
2022-09-28 20:22:56 -0300

    nueva rutadel back y formulario para crear usuario sin foto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const UsersController = require('../controllers/Users');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/users', UsersController);
 
 module.exports = router;
