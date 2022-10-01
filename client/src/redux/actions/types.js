f6cfe800143fe2a92bf03f41f4c9ce43bb5d8bd7
Maria <marjgchs@gmail.com>
2022-10-01 13:52:38 -0300

    filter and post user actions +checklist

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,13 +1,13 @@
 //----------------GAMES----------------//(Main)
 
-export const GET_ALL_GAMES = 'GET_ALL_GAMES';
-export const GET_GAME_QUERY = 'GET_GAME_QUERY';
+export const GET_ALL_GAMES = 'GET_ALL_GAMES'; //✓
+export const GET_GAME_QUERY = 'GET_GAME_QUERY'; //✓
 
-export const GET_GENRES = 'GET_GENRES';
-export const GENRE_FILTER = 'GENRE_FILTER';
+export const GET_GENRES = 'GET_GENRES'; //✓
+export const GENRE_FILTER = 'GENRE_FILTER'; //✓
 
-export const GET_AUDIENCES = 'GET_AUDIENCE';
-export const AUDIENCE_FILTER = 'AUDIENCE_FILTER';
+export const GET_AUDIENCES = 'GET_AUDIENCE'; //✓
+export const AUDIENCE_FILTER = 'AUDIENCE_FILTER'; //✓
 
 export const NAME_ORDER = 'NAME_ORDER';
 
@@ -16,7 +16,7 @@ export const NAME_ORDER = 'NAME_ORDER';
 
 //-------------SINGLE-GAME---------------//(Game Details)
 
-export const GET_GAME = 'GET_GAME';
+export const GET_GAME = 'GET_GAME'; //✓
 
 export const ADD_GAME = 'ADD_GAME';
 export const EDIT_GAME = 'EDIT_GAME';
@@ -25,7 +25,7 @@ export const DELETE_GAME = 'DELETE_GAME';
 //-----------------USER--------------------//(User Details)
 
 export const GET_USER = 'GET_USER';
-export const ADD_USER = 'ADD_USER';
+export const ADD_USER = 'ADD_USER'; //✓
 export const EDIT_USER = 'EDIT_USER';
 export const DELETE_USER = 'DELETE_USER';
 
