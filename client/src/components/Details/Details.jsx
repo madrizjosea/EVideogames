476e4bc67e044f130da74358bdfc23c0ffb40dbe
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-10-02 07:41:55 -0300

    ADD/presentación básica de details

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,8 +1,41 @@
-import React from "react";
-import './Details.module.css'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllGames, getGame } from '../../redux/actions/games';
+import styles from './Details.module.css';
 
 export default function Details() {
-    return(
-        <h1 className="texto">Details del Videojuego</h1>
-    )
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const details = useSelector(state => state.games.game);
+
+  useEffect(() => {
+    dispatch(getGame(id));
+  }, [dispatch, id]);
+
+  return details.id ? (
+    <section className={styles.container}>
+      <div className={styles.leftSide}>
+        <h2>{details.name}</h2>
+        <div className={styles.imageContainer}>
+          <img src={details.image} alt={details.name} />
+        </div>
+      </div>
+      <div className={styles.middle}>
+        <div>
+          <h2>Release Date: {details.releaseDate}</h2>
+          <div className={styles.btns}>
+            <h2>Price: $60</h2>
+            {/* <button>Add to Cart</button>
+            <button>Write review</button> */}
+          </div>
+          <div className={styles.ratings}>
+            <p>{details.audiences[0].name}</p>
+            <p>Rating {details.rating}</p>
+          </div>
+        </div>
+        <p className={styles.description}>{details.description}</p>
+      </div>
+    </section>
+  ) : null;
 }
