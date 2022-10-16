92fbf3097c554dd9b3fdb730e423591bfc2f930c
Maria <marjgchs@gmail.com>
2022-10-16 18:03:10 -0300

    validation new game

--- client/src/components/AddGame/AddGame.jsx
+++ client/src/components/AddGame/AddGame.jsx
@@ -3,10 +3,46 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { addGame } from '../../redux/actions/games/index.js';
 
+function validation(input) {
+    let err = {};
+    if (!input.name) {
+        err.name = 'Can not be empty';
+    } else if (input.name.length > 25) {
+        err.name = 'Should contain less than 25 characters';
+    }
+
+    if (!input.description) {
+        err.description = 'Can not be empty.';
+    }
+
+    if (!input.image) {
+        err.image = 'Can not be empty.';
+    } else if (!/^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(input.image)) {
+        err.image = 'Should be a valid image url.';
+    }
+
+    if (!input.releaseDate) {
+        err.releaseDate = 'Can not be empty.';
+    } else if (!/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(input.releaseDate)) {
+        err.releaseDate = 'Should be a valid date(yyyy-mm-dd).';
+    }
+
+    if (!input.rating) {
+        err.rating = 'Can not be empty.';
+    } else if (!/^[0-9]+$/.test(input.rating)) {
+        err.rating = 'Should only contain numbers.';
+    } else if (!/^(?=.*[1-5])\d+(?:\.[05]0?)?$/.test(input.rating)) {
+        err.rating = 'Should be between 1 and 5(only 0.5 decimals are valid).';
+    }
+
+    return err;
+}
+
 export default function AddVideogame() {
 
     const dispatch = useDispatch();
 
+    const [err, setErr] = useState({});
     const [input, setInput] = useState({
         name: '',
         description: '',
@@ -21,7 +57,11 @@ export default function AddVideogame() {
         setInput({
             ...input, [e.target.name]: e.target.value
         })
+        setErr(validation({
+            ...input, [e.target.name]: e.target.value
+        }))
         console.log(input);
+        console.log(err);
     }
 
     function audiencetHandler(e) {
@@ -45,7 +85,9 @@ export default function AddVideogame() {
                 genres: [...input.genres, e.target.value]
             })
         }
-        console.log(input);
+        e.target.value = 'default';
+        console.log(input.genres);
+        console.log(err.genres);
     }
 
     function deleteHandler(e) {
@@ -59,16 +101,28 @@ export default function AddVideogame() {
 
     function submitHandler(e) {
         e.preventDefault();
-        dispatch(addGame(input));
-        setInput({
-            name: '',
-            description: '',
-            image: '',
-            releaseDate: '',
-            rating: '',
-            audiences: [],
-            genres: [],
-        });
+        if (!err.name &&
+            !err.description &&
+            !err.image &&
+            !err.releaseDate &&
+            !err.rating) {
+
+            alert("New Game has been succesfully added!");
+
+            dispatch(addGame(input));
+
+            setInput({
+                name: '',
+                description: '',
+                image: '',
+                releaseDate: '',
+                rating: '',
+                audiences: [],
+                genres: [],
+            });
+        } else {
+            return alert("Make sure all the fields are correct.");
+        }
     }
 
     return (
@@ -78,29 +132,61 @@ export default function AddVideogame() {
 
                 <h1>New Videogame</h1>
 
-                <p className="labelForm">Name:</p>
+                <div>
+                    <p className="labelForm">Name:</p>
 
-                <div className="err">
-                    <input value={input.name} type="text" name="name" onChange={(e) => changeHandler(e)} />
+                    <div>
+                        <input value={input.name} type="text" name="name" onChange={(e) => changeHandler(e)} />
+                    </div>
+                    {err.name &&
+                        <span className="error">{err.name}</span>
+                    }
                 </div>
 
-                <p className="labelForm">Description:</p>
+                <div>
+                    <p className="labelForm">Description:</p>
 
-                <div className="err">
-                    <input value={input.description} type="text" name="description" onChange={(e) => changeHandler(e)} />
+                    <div>
+                        <input value={input.description} type="text" name="description" onChange={(e) => changeHandler(e)} />
+                    </div>
+                    {err.description &&
+                        <span className="error">{err.description}</span>
+                    }
                 </div>
 
-                <p className="labelForm">Image:</p>
+                <div>
+                    <p className="labelForm">Image:</p>
 
-                <div className="err">
-                    <input value={input.image} type="text" name="image" onChange={(e) => changeHandler(e)} />
+                    <div>
+                        <input value={input.image} type="text" name="image" onChange={(e) => changeHandler(e)} />
+                    </div>
+                    {err.image &&
+                        <span className="error">{err.image}</span>
+                    }
                 </div>
 
-                <p className="labelForm">Release Date:</p>
+                <div>
+                    <p className="labelForm">Release Date:</p>
 
-                <div className="err">
-                    <input value={input.releaseDate} type="text" name="releaseDate" onChange={(e) => changeHandler(e)} />
+                    <div>
+                        <input value={input.releaseDate} type="text" name="releaseDate" onChange={(e) => changeHandler(e)} />
+                    </div>
+                    {err.releaseDate &&
+                        <span className="error">{err.releaseDate}</span>
+                    }
                 </div>
+
+                <div>
+                    <p className="labelForm">Rating:</p>
+
+                    <div >
+                        <input value={input.rating} type="text" name="rating" onChange={(e) => changeHandler(e)} />
+                    </div>
+                    {err.rating &&
+                        <span className="error">{err.rating}</span>
+                    }
+                </div>
+
 
                 <div id="audiences">
                     <div>
